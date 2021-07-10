e5b142859fe2a6288dc0e5a1ab1c643221f8da53
SANDRA1ROY1 <sandraroyaricatt@gmail.com>
2021-07-10 10:51:05 -0400

    Added audio functionality to first screen

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector("#audio").play();
+const audioImageTapped = () =>{
+    console.log("image tapped eneted");
+    if(document.querySelector("#audioImage").classList.contains("paused")){
+        document.querySelector("#audioImage").classList.remove("paused");
+        document.querySelector("#audio").play();
+        document.querySelector("#audioImage").src="assets/audioImage.png";
+
+    }else{
+        
+        document.querySelector("#audioImage").classList.add("paused");
+        document.querySelector("#audio").pause();
+        document.querySelector("#audioImage").src="assets/audioOff.png";
+
+    }
+
+}
+document.querySelector("#audioImage").addEventListener("click",audioImageTapped);
